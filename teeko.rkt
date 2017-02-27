d55545bef6463bb73f0890dc8416cd80b273f2be
first-stinger <justin.michael.stringer@gmail.com>
2017-02-27 11:11:27 -0600

    updating for DrRacket

--- teeko.rkt
+++ teeko.rkt
@@ -2,6 +2,7 @@
 
 (require 2htdp/universe)
 (require 2htdp/image)
+(require test-engine/racket-tests)
 
 ;; 1a. DATA DEFINITIONS
 ;; #1: A Teeko is a structure: (make-Teeko (make-array #(5 5) 'X))
@@ -11,14 +12,9 @@
 (define HEIGHT 150)
 (define EMPTY_SCENE (empty-scene WIDTH HEIGHT))
 (define GAMEBOARD_FONT_SIZE 12)
+(define EMPTY_SQUARE_TEXT_COLOR "black")
 
-
-#|(define tko (make-Teeko (array #[ #['A 'B 'C 'D 'E] 
-				    #['F 'G 'H 'I 'J]
-				    #['K 'L 'M 'N 'O]
-				    #['P 'Q 'R 'S 'T]
-				    #['U 'V 'W 'X 'Y]])))
-
+#|
 tko
 (define tko   '(A B C D E
 		  F G H I J
@@ -34,24 +30,19 @@ tko
 ;; circle; returns an image of the character in a circle of the color 
 ;; consumed.
 ;; 2c. HEADER
-;; (define (image-chr chr clr) EMPTY_SCENE)
+(define (image-chr chr clr) EMPTY_SCENE)
 ;; 3. FUNCTIONAL EXAMPLES & TESTS
-;;(check-expect (image-chr 'A "white") (text "A" GAMEBOARD_FONT_SIZE "white"))
+(check-expect (image-chr 'A "white")
+              (text "A" GAMEBOARD_FONT_SIZE EMPTY_SQUARE_TEXT_COLOR))
 ;;(check-expect (image-chr 'A "white") (circle 15 "solid" "white"))
 ;; 4. TEMPLATE
 ;; (define (image-chr chr clr) 
 ;;   (... chr ... clr ...))
 ;; 5. CODE
-(define (image-chr chr clr) 
-  (circle 15 "solid" clr))
+;;(define (image-chr chr clr)  (circle 15 "solid" clr))
 
-(image-chr "a" "white")
+;;(image-chr "a" "white")
 
-;;mya
-;;(array-indexes-ref mya (array #['#(0 0) '#(1 1)]))
-;;(define arr (array #[#[1 2] #[10 20]]))
-;;(define idxs (array #['#(0 0) '#(1 1)]))
-;;(array-indexes-ref arr idxs)
 
 ;; 1c. FUNCTION WISH LIST
 ;render
@@ -89,3 +80,5 @@ tko
 |#
 
 ;;(teeko-prog (make-Teeko ...))
+
+(test)
